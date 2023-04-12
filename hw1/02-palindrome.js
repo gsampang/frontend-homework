6e7648e5e14dd36bea4ffe69c7a6008e67a6cad5
gsonline628 <gsonline628@gmail.com>
2023-04-12 12:34:00 -0700

    hw1 02.js logic done

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,31 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+//referenced this article: https://www.freecodecamp.org/news/js-basics-how-to-reverse-a-number-9aefc20afa8d/
+function reverseNum(inputNum) {
+  let inputArray = inputNum.split("");
+  let inputReverse = inputArray.reverse();
+  let inputNumReverse = inputReverse.join("");
+  inputNum = inputNumReverse;
+
+  return inputNum;
+}
+
+function handleInput(oninput) {
+  let divElem = document.querySelector("div > div");
+
+  let temp = elem.value;
+  temp = reverseNum(temp);
+
+  if (temp === elem.value) {
+    divElem.textContent = "Yes. This is a palindrome!";
+  } else {
+    divElem.textContent = "No. Try again.";
+  }
+
+  //error message
+  if (parseFloat(elem.value) < 0) {
+    divElem.textContent = "Error: please enter a positive number";
+  }
+}
