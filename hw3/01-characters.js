8ab04820ca8f3775db559329812917b59e0ff5eb
Gabriel Sampang <gsampang@pdx.edu>
2023-05-10 09:47:08 -0700

    HW3 01 figure approach, title not figured out yet

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,41 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+// fetch(url);
+
+let app = document.querySelector("#results");
+
+const fetchData = (url) => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+
+            //Once all data has been fetched, append to container and format
+            //then append container to the DOM
+            let container = document.createElement("div");
+            container.className = "container";
+
+            for (elem of data) {
+                let figureElement = document.createElement("figure");
+                figureElement.className = "figure";
+
+                let imgElement = document.createElement("img");
+                imgElement.src = elem.imageUrl;;
+                imgElement.alt = elem.fullName;
+                figureElement.appendChild(imgElement);
+
+                let figcaptionElement = document.createElement("figcaption");
+                figcaptionElement.textContent = elem.fullName;
+                figureElement.appendChild(figcaptionElement);
+
+                container.appendChild(figureElement);
+                app.append(container);
+            }
+            
+        })
+        .catch((error) => console.error(error))
+        .finally(() => console.log("Finally block, runs regardsless. Fill in"));
+}
+
+fetchData(url);
