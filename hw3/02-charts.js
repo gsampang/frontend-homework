e168f5eab10c0f2b888b8dae643199143e850988
Gabriel Sampang <gsampang@pdx.edu>
2023-05-10 14:36:25 -0700

    HW3 02 First Donut Chart WIP

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -29,17 +29,58 @@ const borderColors = [
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    //Array for renderChart, holds a given house and number of people in it.
+    const groups = [];
+    let i = 0;
+
+    //1. Get an array of all houses (get an array of all unique items from data by family)
+    //From: https://plainenglish.io/blog/how-to-get-distinct-values-from-an-array-of-objects-in-javascript
+    const unique = [...new Set(data.map((elem) => elem.family))];
+
+    for (item of unique) {
+      //2. Filter data by house for each house and get count. (filter data by each item in unique)
+      let matches = data.filter(elem => elem.family === item);
+
+      //Update array for renderChart
+      groups[i] = {house: item, count: matches.length};
+      i++;
+    }
+
+    return groups;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log("Finally block, runs regardsless. Fill in");
+  }
+}
+
+const renderChart = async () => {
   const donutChart = document.querySelector('.donut-chart');
+
+  const data = await fetchData(url);
+  console.log(data);
+
+  const houses = [];
+  const counts = [];
+
+  for (let i = 0; i < data.length; i++) {
+    houses[i] = data[i].house;
+    counts[i] = data[i].count;
+  }
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: houses,
       datasets: [
         {
           label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          data: counts,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -50,3 +91,4 @@ const renderChart = () => {
 };
 
 renderChart();
+
